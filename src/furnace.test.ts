aba2e27879145e28d65bd5b82d1837125994478d
Jamie Wong <jamie.lf.wong@gmail.com>
2024-02-02 14:57:37 -0800

    Add furnace test

--- src/furnace.test.ts
+++ src/furnace.test.ts
@@ -1,13 +1,13 @@
 import { GasFurnace } from "./furnace";
 
 describe("GasFurnace", () => {
-  const furnace = new GasFurnace({
-    afuePercent: 96,
-    capacityBtusPerHour: 80000,
-  });
-
   describe("getThermalResponse", () => {
-    test("max capacity", () => {
+    test("afue 96", () => {
+      const furnace = new GasFurnace({
+        afuePercent: 96,
+        capacityBtusPerHour: 80000,
+      });
+
       const response = furnace.getThermalResponse({
         btusPerHourNeeded: 40000,
         insideAirTempF: 70,
@@ -17,6 +17,40 @@ describe("GasFurnace", () => {
       expect(response.btusPerHour).toBeCloseTo(40000, 0);
       expect(Object.keys(response.fuelUsage)).toEqual(["naturalGasCcfPerHour"]);
       expect(response.fuelUsage.naturalGasCcfPerHour).toBeCloseTo(0.4, 2);
+    });
+
+    test("afue 80", () => {
+      const furnace = new GasFurnace({
+        afuePercent: 80,
+        capacityBtusPerHour: 80000,
+      });
+
+      const response = furnace.getThermalResponse({
+        btusPerHourNeeded: 40000,
+        insideAirTempF: 70,
+        outsideAirTempF: 5,
+      });
+
+      expect(response.btusPerHour).toBeCloseTo(40000, 0);
+      expect(Object.keys(response.fuelUsage)).toEqual(["naturalGasCcfPerHour"]);
+      expect(response.fuelUsage.naturalGasCcfPerHour).toBeCloseTo(0.48, 2);
+    });
+
+    test("over max capacity", () => {
+      const furnace = new GasFurnace({
+        afuePercent: 80,
+        capacityBtusPerHour: 80000,
+      });
+
+      const response = furnace.getThermalResponse({
+        btusPerHourNeeded: 100000,
+        insideAirTempF: 70,
+        outsideAirTempF: 5,
+      });
+
+      expect(response.btusPerHour).toBeCloseTo(80000, 0);
+      expect(Object.keys(response.fuelUsage)).toEqual(["naturalGasCcfPerHour"]);
+      expect(response.fuelUsage.naturalGasCcfPerHour).toBeCloseTo(0.96, 2);
     });
   });
 });
