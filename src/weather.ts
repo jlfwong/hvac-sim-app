5a8c7eb4593684363767890b76a23936aea63d8e
Jamie Wong <jamie.lf.wong@gmail.com>
2024-02-07 17:46:46 -0800

    Weather cleanup

--- src/weather.ts
+++ src/weather.ts
@@ -24,14 +24,13 @@ export class JSONBackedHourlyWeatherSource implements WeatherSource {
 
   constructor(entries: JSONWeatherEntry[]) {
     for (let entry of entries) {
-      const dt = DateTime.fromISO(entry.datetime, { setZone: true });
+      const dt = DateTime.fromISO(entry.datetime);
       this.entryByHour[this.hourKey(dt)] = entry;
     }
-    const dt = DateTime.fromISO(entries[0].datetime);
-    console.log(this.hourKey(dt), dt.toISO());
   }
 
   private hourKey(datetime: DateTime): string {
+    // Make sure we convert to UTC first to get the hour of the day!
     return datetime.toUTC().toFormat("yyyy-LL-dd HH");
   }
 
