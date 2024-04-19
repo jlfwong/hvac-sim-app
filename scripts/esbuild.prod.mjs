3b9d24d24c5f8cb45a951406577a0b369d32c35a
Jamie Wong <jamie.lf.wong@gmail.com>
2024-04-19 00:10:48 -0700

    Install sentry

---
+++ scripts/esbuild.prod.mjs
@@ -0,0 +1,19 @@
+import * as esbuild from 'esbuild'
+import { sentryEsbuildPlugin } from "@sentry/esbuild-plugin"
+
+await esbuild.build({
+  entryPoints: ['src/app/app.tsx'],
+  sourcemap: true,
+  minify: true,
+  bundle: true,
+  outdir: 'dist/js',
+  logLevel: 'info',
+  plugins: [
+    // Put the Sentry esbuild plugin after all other plugins
+    sentryEsbuildPlugin({
+      authToken: process.env.SENTRY_AUTH_TOKEN,
+      org: "jamie-wong",
+      project: "javascript-react",
+    }),
+  ],
+});
